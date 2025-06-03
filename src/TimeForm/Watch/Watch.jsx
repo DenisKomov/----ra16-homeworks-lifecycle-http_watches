7b779d1528a73508f2c31af9c0467ce8470a7e64
DenisKomov <143321805+DenisKomov@users.noreply.github.com>
2025-06-03 09:39:09 +0300

    first commit

---
+++ src/TimeForm/Watch/Watch.jsx
@@ -0,0 +1,43 @@
+import { Component } from 'react'
+import PropTypes from 'prop-types';
+import './Watch.css'
+
+export default class Watch extends Component {
+    constructor(props) {
+        super(props)
+        this.title = props.title;
+        this.zone = props.zone
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            this.setState(new Date())
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    getTime(zone) {
+        const date = new Date(new Date().getTime() + zone * 3600000);
+        return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    }
+
+    render() {
+        return (
+            <div className='watch'>
+                <div className='title'>{this.title}</div>
+                <div className='time'>{this.getTime(this.zone)}</div>
+                <div className='zone'>{this.zone}</div>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+Watch.propTypes = {
+    title: PropTypes.string.isRequired,
+    zone: PropTypes.number.isRequired,
+    children: PropTypes.array.isRequired,
+};
